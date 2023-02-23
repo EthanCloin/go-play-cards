a1b9b393726908ec1cb41883a0cf3f6abab9d5c4
Ethan Cloin <psnethan@gmail.com>
2023-02-22 22:41:51 -0500

    Add explanatory comments and cleanup

--- deck.go
+++ deck.go
@@ -2,22 +2,32 @@ package main
 
 import "fmt"
 
+// a deck represents a slice of playing cards, represented
+// simply as strings of the form "<suit> of <rank>"
 type deck []string
 
-// receiver syntax: (d deck) preceding func name
-// allows all `deck` type to utilize this method
+/* 
+receiver syntax: the '(d deck)' preceding func name
+allows all `deck` type to utilize this method via dot operator
+aka myDeck.print()
+*/
+
+// prints the card value and its zero-based index in deck
 func (d deck) print(){
-	for _, card := range d {
-		fmt.Println(card)
+	for i, card := range d {
+		fmt.Println(i, card)
 	}
 }
 
+// generates a new deck of 52 cards
 func newDeck() deck {
 	suits := []string{"Hearts", "Spades", "Clubs", "Diamonds"}
 	ranks := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
 
 	newDeck := deck{}
 
+	// go compiler refuses to allow any unutilized variables, 
+	// assign any unneeded variables as '_' to allow compilation
 	for _, suit := range suits {
 		for _, rank := range ranks {
 			newDeck = append(newDeck, rank + " of " + suit)
--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import "fmt"
-func main() {
-	// interesting that this fxn declared in deck file doesn't need
-	// explicitly imported. what happens if i have two fxns of same 
-	// name ?
-	cards := newDeck()
-	// cards.print()
 
+func main() {
+	/* 
+	interesting that this fxn declared in deck file doesn't need
+	explicitly imported. 
+	what happens if i have two fxns of same name in different files?
+	*/
+	cards := newDeck()
+
+	// notice multiple values returned from single function <3
 	myHand, cards := dealHand(cards, 5)
 	myHand.print()
-	fmt.Println("REMAINING: ")
+
+	fmt.Println("REMAINING IN DECK: ")
 	cards.print()
 }
